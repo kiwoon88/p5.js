ebae4ca87f1768b0721255170d0fc17fe42e77f0
kiwoonCode <kiwoon88@gmail.com>
2021-04-07 23:48:48 +0900

    Create sketch.js

---
+++ 01. HTML과 JS의 기초/A.04 캔버스와 배경색/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+  //myCnv = createCanvas(400, 400);
+  //noCanvas();
+  
+}
+
+function draw() {
+  background(0, 100, 150);
+  //myCnv.background(255);
+  //background('red');
+  //background(255, 5);  
+  //background(255, 255, 0);
+  //background(255, 255, 0, 5);
+  //myCnv.fill(0, 255, 0);
+  //myCnv.stroke(255, 0, 0);
+  //myCnv.strokeWeight(5);
+  //myCnv.translate(100,100);
+  ellipse(mouseX, mouseY, 50, 50);
+}
