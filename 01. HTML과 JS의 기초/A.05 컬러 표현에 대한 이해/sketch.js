7b8c066e2fed92d53891fc472af77af3aab7d994
kiwoonCode <kiwoon88@gmail.com>
2021-04-07 23:50:24 +0900

    Create sketch.js

---
+++ 01. HTML과 JS의 기초/A.05 컬러 표현에 대한 이해/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  // c = color(255, 150, 150, 255);
+  // console.log(c);
+}
+
+function draw() {
+  background(255);
+  // fill(0, 200, 0);
+  // stroke(0, 0, 255);
+  // square(100, 100, 200);
+  // fill(160,0,160,50);
+  circle(mouseX, mouseY, 50);
+}
