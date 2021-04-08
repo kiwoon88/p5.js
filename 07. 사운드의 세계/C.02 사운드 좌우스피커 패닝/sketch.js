33227b3cb4901a933327704cbbdfb01168155bfd
kiwoonCode <kiwoon88@gmail.com>
2021-04-08 09:33:48 +0900

    Create sketch.js

---
+++ 07. 사운드의 세계/C.02 사운드 좌우스피커 패닝/sketch.js
@@ -0,0 +1,37 @@
+let setting = 0.3;
+let song;
+let img;
+let btn;
+let vol;
+let pan=0;
+
+function preload(){
+  img = loadImage("data/oldpop.jpg");
+  song = loadSound("data/popsong.mp3");
+}  
+
+function setup() {
+  createCanvas(280, 278);
+  btn = createButton("PLAY");
+  btn.mousePressed(music);
+  vol = createSlider(0,1,setting,0.01);
+  pan = createSlider(-1.0,1.0,0,0.01);
+  background(255);
+}
+
+function music(){
+  if(song.isPlaying()){ //연주중이라면~
+    song.pause(); //노래 멈춤
+    btn.html("PLAY");
+    background(255);
+  }else{
+    song.play(); //노래시작
+    btn.html("PAUSE");
+    image(img,0,0);
+  }
+}
+
+function draw() {
+  song.setVolume(vol.value());
+  song.pan(pan.value());
+}
