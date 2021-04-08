f015a059115cccc83507c7ca937ad26d86ed005a
kiwoonCode <kiwoon88@gmail.com>
2021-04-08 13:38:25 +0900

    Create baundary.js

---
+++ 11. nature of code(자연의 모방과 물리수학)/V.11 물질과 입자의 세계2(matter.js)/baundary.js
@@ -0,0 +1,23 @@
+class Boundary {
+  constructor(x, y, w, h){
+    let options = {
+      isStatic: true
+    }
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+  
+  show(){
+    fill(255);
+    stroke(255);
+    let pos = this.body.position;
+    push();    
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
+}
