e5eefd518afccef0ca7a18fec4f2de602bdd5e5d
kiwoonCode <kiwoon88@gmail.com>
2021-04-08 13:40:23 +0900

    Create sketch.js

---
+++ 12. 머신러닝라이브러리(ml5.js)/M.01 ml5. 이미지분류(MobileNet) 기초/sketch.js
@@ -0,0 +1,35 @@
+let img;
+let imgClassifier;
+
+function preload(){
+  img = loadImage('data/animal1.jpg');
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+  image(img, 0, 0);
+  imgClassifier = ml5.imageClassifier('MobileNet', modelReady);
+}
+
+function modelReady(){
+  console.log('이미지분류 모델 로드 완료');
+  imgClassifier.classify(img, gotResult);
+}
+
+function gotResult(err, results){
+  if(err){
+    console.error(err);
+    return;
+  }
+  
+  console.log(results);
+  let label = results[0].label;
+  let confidence = results[0].confidence;
+  textAlign(CENTER);
+  textSize(20);
+  stroke(0);
+  strokeWeight(5);
+  fill('#FFFF00');
+  text(label, width/2, height-30)
+  text(nf(confidence*100, 0, 1)+' %', width/2, height-5);
+}
