6f4e66eab067f4ea7e1db4558d6f45e2fc351365
kiwoonCode <kiwoon88@gmail.com>
2021-04-08 13:44:57 +0900

    Create sketch.js

---
+++ 12. 머신러닝라이브러리(ml5.js)/M.03 ml5. 드롭박스 이용 이미지분류(MobileNet)/sketch.js
@@ -0,0 +1,49 @@
+let img;
+let cnv;
+let imgClassifier;
+
+
+function setup() {
+  cnv = createCanvas(100, 100);
+  imgClassifier = ml5.imageClassifier('MobileNet', modelReady);
+  cnv.drop(fileLoaded);
+  background(200);
+}
+
+function modelReady(){
+  console.log('이미지분류 모델 로드 완료');
+}
+
+function fileLoaded(file){
+  if(file.type == 'image'){
+    img = createImg(file.data, '', '', imgLoaded);
+  }else{
+    alert('이미지 파일을 선택하십시오');
+  }
+}
+
+function imgLoaded(){
+  resizeCanvas(img.width, img.height);
+  image(img, 0, 0);  
+  img.size(100, 100);
+
+  imgClassifier.classify(img, gotResult);
+}
+
+function gotResult(err, results){
+  if(err){
+    console.error(err);
+    return;
+  }
+  
+  console.log(results);
+  let label = results[0].label;
+  let confidence = results[0].confidence;
+  textAlign(CENTER);
+  textSize(20);
+  stroke(0);
+  strokeWeight(5);
+  fill('#FFFF00');
+  text(label, width/2, height-30)
+  text(nf(confidence*100, 0, 1)+' %', width/2, height-5);
+}
