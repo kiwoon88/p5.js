ec884e67202bfb8de1014dd105af70a503b2a3c9
kiwoonCode <kiwoon88@gmail.com>
2021-04-08 14:20:37 +0900

    Create sketch.js

---
+++ 12. 머신러닝라이브러리(ml5.js)/M.15 ml5. 얼굴 이미지 표정 인식(facemesh)/sketch.js
@@ -0,0 +1,31 @@
+let img;
+let facemesh;
+let predictions;
+
+function preload(){
+  img = loadImage('face.png', imageReady);
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+  image(img,0,0);
+}
+
+function imageReady(){
+  facemesh = ml5.facemesh(modelReady);
+  facemesh.on('predict', gotResult);
+}
+
+function gotResult(results){
+  console.log(results);
+  predictions = results[0].scaledMesh;
+  for(let i=0; i<predictions.length; i++){
+    fill(0,255,0);
+    circle(predictions[i][0], predictions[i][1], 3);
+  }
+}
+
+function modelReady(){
+  console.log('Facemesh 모델 준비 완료');
+  facemesh.predict(img);
+}
